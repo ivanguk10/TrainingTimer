60585265b42539bd2cbaa4538972d6fd1e8b593a
ivanguk10 <48579825+ivanguk10@users.noreply.github.com>
2021-02-10 21:03:54 +0300

    setiing enum

---
+++ app/src/main/java/com/example/tabatatimer/enums/Setting.kt
@@ -0,0 +1,10 @@
+package com.example.tabatatimer.enums
+
+enum class Setting(val setting: String) {
+    FONTSIZE("fontSize"),
+    THEME("theme"),
+    LANG("text_lang"),
+    DELETE_ALL("DeleteAll");
+
+
+}
